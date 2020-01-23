e7bfc8dcfcf9cedd3cee52fbe1948b513ffcc9eb
cvevera <cvevera@gmail.com>
2020-01-23 18:37:03 -0500

    intial commit

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+
+$(".btn").click(function () {
+    $(".first-text").hide();
+    $(".first-paragraph").hide();
+    $(".first-button").hide();
+    console.log("test")
+    $("#projects-container").show();
+    // $(".picture").show();
+});
