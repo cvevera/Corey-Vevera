1beb31a5b3afffd2214b3379dff2acd4c0750a08
cvevera <cvevera@gmail.com>
2020-03-11 14:21:42 -0400

    updated site, styling, and about me

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,37 @@
+$(document).ready(function () {
 
-$(".btn").click(function () {
+$(document).on("click", ".btn", projects);
+$(document).on("click", "#projects-nav", projects);
+$(document).on("click", "#home-nav", home);
+$(document).on("click", "#about-nav", aboutMe);
+
+function projects () {
     $(".first-text").hide();
     $(".first-paragraph").hide();
     $(".first-button").hide();
-    console.log("test")
+    $("#aboutMe").hide();
     $("#projects-container").show();
     // $(".picture").show();
+}
+
+function home () {
+    $("#projects-container").hide();
+    $("#aboutMe").hide();
+    $(".first-text").show();
+    $(".first-paragraph").show();
+    $(".first-button").show();
+
+}
+
+function aboutMe () {
+    $(".first-text").hide();
+    $(".first-paragraph").hide();
+    $(".first-button").hide();
+    $("#projects-container").hide();
+    $("#aboutMe").show();
+}
+
+
 });
+
+
